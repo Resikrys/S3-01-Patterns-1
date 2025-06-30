61d94fa6c50b06688512d77c2d71b78c29342063
resikrys <resicris@gmail.com>
2025-06-30 13:38:42 +0200

    structure project

---
+++ src/main/java/s3_01_singleton/InvalidCommandException.java
@@ -0,0 +1,4 @@
+package s3_01_singleton;
+
+public class InvalidCommandException {
+}
--- src/main/java/s3_01_singleton/Main.java
+++ src/main/java/s3_01_singleton/Main.java
@@ -3,7 +3,7 @@ package s3_01_singleton;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // manager.start() or manager.run()
 
     }
 }
