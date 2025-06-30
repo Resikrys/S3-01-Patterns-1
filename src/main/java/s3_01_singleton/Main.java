cf9fd9db1edd052097ae3c7ef1df55428b045d91
resikrys <resicris@gmail.com>
2025-06-30 13:35:34 +0200

    add singleton package

---
+++ src/main/java/s3_01_singleton/Main.java
@@ -0,0 +1,10 @@
+package s3_01_singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+}
+
