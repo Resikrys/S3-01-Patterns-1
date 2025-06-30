d216451bb335ece099c6de79ca40a5de37dfe96c
resikrys <resicris@gmail.com>
2025-06-30 13:42:16 +0200

    basic singleton structure

--- src/main/java/s3_01_singleton/Undo.java
+++ src/main/java/s3_01_singleton/Undo.java
@@ -1,4 +1,22 @@
 package s3_01_singleton;
 
 public class Undo {
+    private static Undo instance;
+
+    private Undo() {
+        System.out.println(" Undo instance created (only once!)");
+    }
+
+    public static Undo getInstance() {
+        if (instance == null) {
+            instance = new Undo();
+        }
+        return instance;
+    }
+
+    // Class methods Undo (add, delete, history)
+    public void addCommand(String command) {
+        // Lógica para añadir comando
+        System.out.println("Comando añadido: " + command);
+    }
 }
