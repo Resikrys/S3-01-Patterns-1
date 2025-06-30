d5fc212f247b84ab841ffd0df5d873a3d165f107
resikrys <resicris@gmail.com>
2025-06-30 13:59:31 +0200

    add first undo methods

--- src/main/java/s3_01_singleton/Undo.java
+++ src/main/java/s3_01_singleton/Undo.java
@@ -1,10 +1,15 @@
 package s3_01_singleton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Undo {
     private static Undo instance;
+    private List<String> commandHistory;
 
     private Undo() {
         System.out.println(" Undo instance created (only once!)");
+        commandHistory = new ArrayList<>();
     }
 
     public static Undo getInstance() {
@@ -14,9 +19,27 @@ public class Undo {
         return instance;
     }
 
-    // Class methods Undo (add, delete, history)
+    // Class methods Undo (add, delete, history) + check & delete
+
     public void addCommand(String command) {
-        // Lógica para añadir comando
-        System.out.println("Comando añadido: " + command);
+        commandHistory.add(command);
     }
+
+    public void undoLastCommand() {
+        if (!commandHistory.isEmpty()) {
+            String removedCommand = commandHistory.remove(commandHistory.size() - 1);
+            System.out.println("Undone command: " + removedCommand);
+        } else {
+            System.out.println("No commands to undo.");
+        }
+    }
+
+    public  void undoLastCommand() {
+        if(!commandHistory.isEmpty()){
+            commandHistory.remove(commandHistory.size() - 1);
+        }else{
+            System.out.println(" The list is empty");
+        }
+    }
+
 }
