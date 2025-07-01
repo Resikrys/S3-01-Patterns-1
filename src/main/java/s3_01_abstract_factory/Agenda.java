8df9ef2120e023a49f598db30d572eeffe36041b
resikrys <resicris@gmail.com>
2025-07-01 13:23:48 +0200

    add basic structure project files

---
+++ src/main/java/s3_01_abstract_factory/Agenda.java
@@ -0,0 +1,27 @@
+package s3_01_abstract_factory;
+
+public class Agenda {
+    private List<InternationalAddress> addresses;
+    private List<InternationalPhoneNumber> phoneNumbers;
+
+    public Agenda() {
+        this.addresses = new ArrayList<>();
+        this.phoneNumbers = new ArrayList<>();
+    }
+
+    // Método para añadir un contacto utilizando la fábrica
+    public void addContact(ContactFactory factory, /* parámetros para construir la dirección y el teléfono */) {
+        InternationalAddress address = factory.createAddress(/* ... */);
+        InternationalPhoneNumber phoneNumber = factory.createPhoneNumber(/* ... */);
+
+        // Aquí podrías configurar los atributos del address y phoneNumber
+        // address.setStreet("...");
+        // phoneNumber.setPrefix("...");
+
+        this.addresses.add(address);
+        this.phoneNumbers.add(phoneNumber);
+        System.out.println("Contacto añadido: " + address.getFormattedAddress() + ", " + phoneNumber.getFormattedNumber());
+    }
+
+    // ... otros métodos de la agenda
+}
--- src/main/java/s3_01_abstract_factory/Main.java
+++ src/main/java/s3_01_abstract_factory/Main.java
@@ -3,5 +3,14 @@ package s3_01_abstract_factory;
 public class Main {
     public static void main(String[] args) {
         //My code
+        Agenda agenda = new Agenda();
+
+        // Crear un contacto español
+        ContactFactory spanishFactory = new SpanishContactFactory();
+        agenda.addContact(spanishFactory, /* datos para el contacto español */);
+
+        // Crear un contacto de EE. UU.
+        ContactFactory usFactory = new USContactFactory();
+        agenda.addContact(usFactory, /* datos para el contacto de EE. UU. */);
     }
 }
