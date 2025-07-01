c8a995f19af78b6ef778ef149c0ef7fa461677c3
resikrys <resicris@gmail.com>
2025-07-01 10:26:41 +0200

    add optional methods get&delete + clear history

--- src/main/java/s3_01_singleton/Undo.java
+++ src/main/java/s3_01_singleton/Undo.java
@@ -8,18 +8,25 @@ public class Undo {
     private List<String> commandHistory;
 
     private Undo() {
-        System.out.println(" Undo instance created (only once!)");
+        System.out.println("Undo instance created (only once!)");
         commandHistory = new ArrayList<>();
     }
 
     public static Undo getInstance() {
+        //In a production environment, you would consider thread-safety here (like the example)
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException ex) {
+//            ex.printStackTrace();
+//        }
+//        this.value = value;
         if (instance == null) {
             instance = new Undo();
         }
         return instance;
     }
 
-    // Class methods Undo (add, delete, history) + check & delete
+    // Class methods Undo (add command, delete last, see history) + check & delete, clear history
 
     public void addCommand(String command) {
         commandHistory.add(command);
@@ -34,12 +41,29 @@ public class Undo {
         }
     }
 
-    public  void undoLastCommand() {
-        if(!commandHistory.isEmpty()){
-            commandHistory.remove(commandHistory.size() - 1);
-        }else{
-            System.out.println(" The list is empty");
+    public void seeListCommands() {
+        if (commandHistory.isEmpty()) {
+            System.out.println("No commands in history.");
+        } else {
+            System.out.println("Command history:");
+            for (int i = 0; i < commandHistory.size(); i++) {
+                System.out.println((i + 1) + ": " + commandHistory.get(i));
+            }
+            System.out.println("-----------------------------\n");
         }
+    }
+
+    public void checkAndDelete(String commandToDelete) {
+        if (commandHistory.remove(commandToDelete)) {
+            System.out.println("Command erased: '" + commandToDelete + "'");
+        } else {
+            System.out.println("Command '" + commandToDelete + "' not found in history.");
+        }
+    }
+
+    public void clearHistory() {
+        commandHistory.clear();
+        System.out.println("Command history cleaned.");
     }
 
 }
