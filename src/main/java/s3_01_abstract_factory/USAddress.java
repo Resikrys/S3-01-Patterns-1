eedf49cb14ee92b5e97f3cab2e6e64308b4c8b7e
resikrys <resicris@gmail.com>
2025-07-02 12:53:55 +0200

    fix US classes

--- src/main/java/s3_01_abstract_factory/USAddress.java
+++ src/main/java/s3_01_abstract_factory/USAddress.java
@@ -3,39 +3,37 @@ package s3_01_abstract_factory;
 public class USAddress implements InternationalAddress {
     private String street;
     private String number;
-    private String floor;
-    private String portal;
+    private String apartment;
     private String city;
     private String state;
     private String zipCode;
     private String country;
 
-    public USAddress(String street, String number, String floor, String portal, String city, String state, String zipCode, String country) {
+    public USAddress(String street, String number, String apartment, String city, String state, String zipCode, String country) {
         this.street = street;
         this.number = number;
-        this.floor = floor;
-        this.portal = portal;
+        this.apartment = apartment;
         this.city = city;
+        this.state = state;
         this.zipCode = zipCode;
         this.country = country;
     }
 
+    @Override
     public String getStreet() {
         return street;
     }
 
+    @Override
     public String getNumber() {
         return number;
     }
 
-    public String getFloor() {
-        return floor;
+    public String getApartment() {
+        return apartment;
     }
 
-    public String getPortal() {
-        return portal;
-    }
-
+    @Override
     public String getCity() {
         return city;
     }
@@ -48,13 +46,14 @@ public class USAddress implements InternationalAddress {
         return zipCode;
     }
 
+    @Override
     public String getCountry() {
         return country;
     }
 
     @Override
     public String getFormattedAddress() {
-        return "St/ " + getStreet() + ", " + getNumber() + " " + getFloor() + " " + getPortal() + ", "
+        return "St/ " + getStreet() + ", " + getNumber() + " " + getApartment() + ", "
                 + getCity() + getState() +  " (" + getZipCode() + ") - " + getCountry();
     }
 
--- src/main/java/s3_01_abstract_factory/USContactFactory.java
+++ src/main/java/s3_01_abstract_factory/USContactFactory.java
@@ -2,13 +2,24 @@ package s3_01_abstract_factory;
 
 public class USContactFactory implements ContactFactory {
     @Override
-    public InternationalAddress createAddress(String street, String number, String floor, String portal, String city, String state,
-                                              String zipCode, String country) {
-        return new USAddress(street, number, floor, portal, city, state,  zipCode, country);
+    public InternationalAddress createAddress(AddressDataDTO data) {
+        return new USAddress(
+                data.getStreet(),
+                data.getNumber(),
+                data.getApartment(), // Usamos apartment para EE. UU.
+                data.getCity(),
+                data.getState(),     // ¡Usamos state para EE. UU.!
+                data.getZipCode(),   // Usamos zipCode para EE. UU.
+                data.getCountry()
+        );
     }
 
     @Override
-    public InternationalPhoneNumber createPhoneNumber(String prefix, String number, InternationalPhoneNumber.Type type) {
-        return new USPhoneNumber(prefix, number, type);
+    public InternationalPhoneNumber createPhoneNumber(PhoneNumberDataDTO data) {
+        return new USPhoneNumber(
+                data.getPrefix(),
+                data.getPhoneNumber(),
+                data.getType()
+        );
     }
 }
