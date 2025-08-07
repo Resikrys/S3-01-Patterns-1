c761b3babd12c1d5f108b14712d17f7ca48213c2
resikrys <resicris@gmail.com>
2025-08-07 10:52:32 +0200

    upd Agenda - SRP -> separate logic & presentation adding value returns

--- src/main/java/s3_01_abstract_factory/Agenda.java
+++ src/main/java/s3_01_abstract_factory/Agenda.java
@@ -18,76 +18,95 @@ public class Agenda {
         Contact newContact = new Contact(address, phoneNumber);
         this.contacts.add(newContact);
 
-        System.out.println("--- Contact Added ---");
-        System.out.println("Address: " + address.getFormattedAddress());
-        System.out.println("Phone number: " + phoneNumber.getFormattedPhone()); // Assuming getFormattedNumber()
-        System.out.println("------------------------");
+//        System.out.println("--- Contact Added ---");
+//        System.out.println("Address: " + address.getFormattedAddress());
+//        System.out.println("Phone number: " + phoneNumber.getFormattedPhone()); // Assuming getFormattedNumber()
+//        System.out.println("------------------------");
 
         return newContact;
     }
 
 
-public void viewAllContacts() {
-    if (contacts.isEmpty()) {
-        System.out.println("\nEmpty agenda.");
-        return;
-    }
-    System.out.println("\n--- All contacts: ---");
-    for (int i = 0; i < contacts.size(); i++) {
-        Contact contact = contacts.get(i);
-        System.out.println("Contact #" + (i + 1));
-        System.out.println("  Address: " + contact.getAddress().getFormattedAddress());
-        System.out.println("  Phone number: " + contact.getPhoneNumber().getFormattedPhone());
-        System.out.println("---------------------------");
-    }
+//public void viewAllContacts() {
+//    if (contacts.isEmpty()) {
+//        System.out.println("\nEmpty agenda.");
+//        return;
+//    }
+//    System.out.println("\n--- All contacts: ---");
+//    for (int i = 0; i < contacts.size(); i++) {
+//        Contact contact = contacts.get(i);
+//        System.out.println("Contact #" + (i + 1));
+//        System.out.println("  Address: " + contact.getAddress().getFormattedAddress());
+//        System.out.println("  Phone number: " + contact.getPhoneNumber().getFormattedPhone());
+//        System.out.println("---------------------------");
+//    }
+//}
+public List<Contact> viewAllContacts() {
+    return new ArrayList<>(this.contacts);
 }
 
 
-public boolean deleteContact(int index) {
+//public boolean deleteContact(int index) {
+//    if (index >= 0 && index < contacts.size()) {
+//        Contact removedContact = contacts.remove(index);
+//        System.out.println("\n--- Deleted Contact ---");
+//        System.out.println("Erased: " + removedContact.getAddress().getFormattedAddress() +
+//                " / " + removedContact.getPhoneNumber().getFormattedPhone());
+//        System.out.println("-------------------------");
+//        return true;
+//    }
+//    System.out.println("\nInvalid contact index. Can't be removed.");
+//    return false;
+//}
+public Contact deleteContact(int index) {
     if (index >= 0 && index < contacts.size()) {
-        Contact removedContact = contacts.remove(index);
-        System.out.println("\n--- Deleted Contact ---");
-        System.out.println("Erased: " + removedContact.getAddress().getFormattedAddress() +
-                " / " + removedContact.getPhoneNumber().getFormattedPhone());
-        System.out.println("-------------------------");
-        return true;
+        return contacts.remove(index);
     }
-    System.out.println("\nInvalid contact index. Can't be removed.");
-    return false;
+    return null;
 }
 
-public void filterContacts(String countryType) {
-    System.out.println("\n--- Contacts Filtered by " + countryType.toUpperCase() + " ---");
-    boolean found = false;
-
-    for (int i = 0; i < contacts.size(); i++) {
-        Contact contact = contacts.get(i);
-        InternationalAddress address = contact.getAddress();
-        InternationalPhoneNumber phoneNumber = contact.getPhoneNumber();
-
-        if ("spanish".equalsIgnoreCase(countryType)) {
-            if (address instanceof SpanishAddress && phoneNumber instanceof SpanishPhoneNumber) {
-                System.out.println("Contact #" + (i + 1));
-                System.out.println("  Address: " + address.getFormattedAddress());
-                System.out.println("  Phone number: " + phoneNumber.getFormattedPhone());
-                System.out.println("---------------------------");
-                found = true;
-            }
-        } else if ("us".equalsIgnoreCase(countryType)) {
-            if (address instanceof USAddress && phoneNumber instanceof USPhoneNumber) {
-                System.out.println("Contact #" + (i + 1));
-                System.out.println("  Address: " + address.getFormattedAddress());
-                System.out.println("  Phone number: " + phoneNumber.getFormattedPhone());
-                System.out.println("---------------------------");
-                found = true;
-            }
+//public void filterContacts(String countryType) {
+//    System.out.println("\n--- Contacts Filtered by " + countryType.toUpperCase() + " ---");
+//    boolean found = false;
+//
+//    for (int i = 0; i < contacts.size(); i++) {
+//        Contact contact = contacts.get(i);
+//        InternationalAddress address = contact.getAddress();
+//        InternationalPhoneNumber phoneNumber = contact.getPhoneNumber();
+//
+//        if ("spanish".equalsIgnoreCase(countryType)) {
+//            if (address instanceof SpanishAddress && phoneNumber instanceof SpanishPhoneNumber) {
+//                System.out.println("Contact #" + (i + 1));
+//                System.out.println("  Address: " + address.getFormattedAddress());
+//                System.out.println("  Phone number: " + phoneNumber.getFormattedPhone());
+//                System.out.println("---------------------------");
+//                found = true;
+//            }
+//        } else if ("us".equalsIgnoreCase(countryType)) {
+//            if (address instanceof USAddress && phoneNumber instanceof USPhoneNumber) {
+//                System.out.println("Contact #" + (i + 1));
+//                System.out.println("  Address: " + address.getFormattedAddress());
+//                System.out.println("  Phone number: " + phoneNumber.getFormattedPhone());
+//                System.out.println("---------------------------");
+//                found = true;
+//            }
+//        }
+//    }
+//
+//    if (!found) {
+//        System.out.println("No contacts of this type were found '" + countryType + "'.");
+//    }
+//    System.out.println("------------------------------------");
+//}
+public List<Contact> filterContacts(String countryCode) {
+    List<Contact> filteredContacts = new ArrayList<>();
+    for (Contact contact : contacts) {
+        // Se usará el nuevo método getCountryCode() en el producto
+        if (contact.getAddress().getCountryCode().equalsIgnoreCase(countryCode)) {
+            filteredContacts.add(contact);
         }
     }
-
-    if (!found) {
-        System.out.println("No contacts of this type were found '" + countryType + "'.");
-    }
-    System.out.println("------------------------------------");
+    return filteredContacts;
 }
 
 }
